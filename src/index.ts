52ff1709369179d249dae90da9c228cb8f525477
Sachin Singh <contactsachinsingh@gmail.com>
2023-07-01 13:28:55 +0530

    Added string logic to separate function

--- src/index.ts
+++ src/index.ts
@@ -1,26 +1,33 @@
+export function htmlString(
+  structure: TemplateStringsArray,
+  ...variables: any[]
+) {
+  let str = '';
+  if (variables.length === 0) {
+    str = structure.join('');
+  } else {
+    variables.forEach((v, i) => {
+      if (['number', 'string', 'boolean'].includes(typeof v)) {
+        str += `${structure[i]}${v}`;
+      }
+      if (v == null) {
+        // handles both null and undefined
+        str += structure[i];
+      }
+      if (v && typeof v === 'object') {
+        str += `${structure[i]}${JSON.stringify(v)}`;
+      }
+    });
+    str += structure[structure.length - 1];
+  }
+  return str;
+}
+
 export function html(
   structure: TemplateStringsArray,
   ...variables: any[]
 ): DocumentFragment {
-  let htmlString = '';
-  if (variables.length === 0) {
-    htmlString = structure.join('');
-  } else {
-    variables.forEach((v, i) => {
-      if (['number', 'string', 'boolean'].includes(typeof v)) {
-        htmlString += `${structure[i]}${v}`;
-      }
-      if (v == null) {
-        // handles both null and undefined
-        htmlString += structure[i];
-      }
-      if (v && typeof v === 'object') {
-        htmlString += `${structure[i]}${JSON.stringify(v)}`;
-      }
-    });
-    htmlString += structure[structure.length - 1];
-  }
   const template = document.createElement('template');
-  template.innerHTML = htmlString;
+  template.innerHTML = htmlString(structure, ...variables);
   return template.content;
 }
