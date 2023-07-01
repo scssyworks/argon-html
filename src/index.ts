b4cc586061b751d2f0ffc53c76ffcd61b175757e
Sachin Singh <contactsachinsingh@gmail.com>
2023-07-01 14:36:45 +0530

    Added handling of arrays at root level

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,20 @@
+function isObject(item: any) {
+  return item && typeof item === 'object';
+}
+
+function handleArray(arr: any[]): string {
+  return arr.reduce((prev: string, item: any) => {
+    const p = prev ? `${prev} ` : prev;
+    if (item == null) {
+      return p.trim();
+    }
+    if (isObject(item)) {
+      return `${p}${JSON.stringify(item)}`;
+    }
+    return `${p}${item}`;
+  }, '');
+}
+
 export function htmlString(
   structure: TemplateStringsArray,
   ...variables: any[]
@@ -14,8 +31,12 @@ export function htmlString(
         // handles both null and undefined
         str += structure[i];
       }
-      if (v && typeof v === 'object') {
-        str += `${structure[i]}${JSON.stringify(v)}`;
+      if (isObject(v)) {
+        if (Array.isArray(v)) {
+          str += `${structure[i]}${handleArray(v)}`;
+        } else {
+          str += `${structure[i]}${JSON.stringify(v)}`;
+        }
       }
     });
     str += structure[structure.length - 1];
